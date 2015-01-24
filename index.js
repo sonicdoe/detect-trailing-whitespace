82f09fc337ae951e1904cbbbcd086d9e4d9464ce
Jakob Krigovsky <jakob.krigovsky@gmail.com>
2015-01-24 18:34:42 +0100

    Implement main functionality

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = function(str) {
+	if(typeof str !== 'string') {
+		throw new TypeError('Argument to detect-trailing-whitespace must be a string');
+	}
+
+	return /[ \t]+$/m.test(str);
+}
